369919cd08321a9cd5852edecf55848218fa2bd5
Toba-dean <tobaogundimu@gmail.com>
2024-06-20 12:51:18 +0100

    done with the landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Recursive } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/NavBar";
+import { Footer } from "@/components/Footer";
 
 const recursive = Recursive({ subsets: ["latin"] });
 
@@ -19,7 +20,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={recursive.className}>
         <NavBar />
-        {children}
+
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">{children}</div>
+
+          <Footer />
+        </main>
       </body>
     </html>
   );

15a21ba87335ee98b34edb8517e5742ed1975782
Toba-dean <tobaogundimu@gmail.com>
2024-06-28 08:51:45 +0100

    .

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -76,7 +76,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "CaseCobra blackboydean96@gmail.com>",
+        from: "CaseCobra <blackboydean96@gmail.com>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
